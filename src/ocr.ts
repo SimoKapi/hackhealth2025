9ef17f2bb5c88481b76650bb06b2d19c91ae1121
SimoKapi <simon.kapicka@gmail.com>
2025-03-08 21:07:17 +0100

    Server-side ocr-config

--- src/ocr.ts
+++ src/ocr.ts
@@ -5,6 +5,7 @@ const image = "impella.png"
 
 const imgPath = path.join(testDirectory, image)
 
+import { Jimp } from 'jimp';
 import { createWorker } from 'tesseract.js';
 // const rectangles: {
 //     name: string,
@@ -101,24 +102,32 @@ import { createWorker } from 'tesseract.js';
 // ];
 
 export async function ocr_image(imgPath : string, rectangles: {name: string, unit: string|undefined, rectangle: Tesseract.Rectangle}[]) {
-    const worker = await createWorker('eng', 1, {
-        logger(arg) {
-            // console.log(arg)
-        },
-    });
-    await worker.setParameters({
-        tessedit_char_whitelist: '0123456789.-/'
-    });
-    const values = [];
-    for (const field of rectangles) {
-        const { data: { text } } = await worker.recognize(imgPath, { rectangle: field.rectangle });
-        // values.push(`${field.name}: ${text.trim()}`);//${field.unit}
-        values.push({
-            id: field.name,
-            value: text.trim().replaceAll("/", "")
-        })
-    }
-    // console.log(values);
-    await worker.terminate();
-    return values;
+    try {
+        const worker = await createWorker('eng', 1, {
+            logger(arg) {
+                // console.log(arg)
+            },
+        });
+        await worker.setParameters({
+            tessedit_char_whitelist: '0123456789.-/'
+        });
+        const values = [];
+        for (const field of rectangles) {
+            const im = await Jimp.read(imgPath)
+            if (field.rectangle.top + field.rectangle.height > im.height) { field.rectangle.height = im.height - field.rectangle.top }
+            if (field.rectangle.left + field.rectangle.width > im.width) { field.rectangle.width = im.width - field.rectangle.left }
+            const { data: { text } } = await worker.recognize(imgPath, { rectangle: field.rectangle });
+            // values.push(`${field.name}: ${text.trim()}`);//${field.unit}
+            values.push({
+                id: field.name,
+                value: text.trim().replaceAll("/", "")
+            })
+        }
+        // console.log(values);
+        await worker.terminate();
+        return values;
+    } catch (exception: any) {
+        console.error(exception)
+        return null
+    } 
 };
