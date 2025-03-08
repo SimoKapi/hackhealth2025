a2f5aac1599677834435626c6e1d6fe965e65e0d
SimoKapi <simon.kapicka@gmail.com>
2025-03-08 09:44:29 +0100

    Bounding box tool

--- src/ocr.ts
+++ src/ocr.ts
@@ -1,7 +1,7 @@
 var path = require('path');
 
 const testDirectory = "src/test_images"
-const image = "sucky-adjusted.png"
+const image = "sucky-stretchy.png"
 
 const imgPath = path.join(testDirectory, image)
 
@@ -16,50 +16,50 @@ const rectangles: {
     {
         name: "V",
         rectangle: {
-            left: 1050,
-            top: 1274,
-            width: 400,
-            height: 166,
+            left: 774,
+            top: 1961,
+            width: 496,
+            height: 228,
         },
         unit: "lpm"
     },
     {
         name: "Pven",
         rectangle: {
-            left: 1050,
-            top: 1274,
-            width: 400,
-            height: 166,
+            left: 754,
+            top: 2164,
+            width: 501,
+            height: 268,
         },
         unit: "mmHg"
     },
     {
         name: "Pint",
         rectangle: {
-            left: 1050,
-            top: 1274,
-            width: 400,
-            height: 166,
+            left: 769,
+            top: 2428,
+            width: 496,
+            height: 253,
         },
         unit: "mmHg"
     },
     {
         name: "Part",
         rectangle: {
-            left: 1050,
-            top: 1274,
-            width: 400,
-            height: 166,
+            left: 1564,
+            top: 2189,
+            width: 516,
+            height: 243,
         },
         unit: "mmHg"
     },
     {
         name: "Rotation",
         rectangle: {
-            left: 1050,
-            top: 1274,
-            width: 400,
-            height: 166,
+            left: 1549,
+            top: 1956,
+            width: 546,
+            height: 208,
         },
         unit: "rpm"
     },
@@ -72,18 +72,8 @@ const rectangles: {
     const values = [];
     for (const field of rectangles) {
         const { data: { text } } = await worker.recognize(imgPath, { rectangle: field.rectangle });
-        values.push(`${field.name}: ${text} ${field.unit}`);
+        values.push(`${field.name}: ${text.trim()} ${field.unit}`);
     }
     console.log(values);
     await worker.terminate();
 })();
-
-// (async () => {
-//     await worker.setParameters({
-//         tessedit_char_whitelist: '0123456789',
-//       });
-    
-//     const { data: { text } } = await worker.recognize(imgPath);
-//     console.log(text);
-//     await worker.terminate();
-//   })();
